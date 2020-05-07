992e832e098ee68f43d7a88ee52c33e04b06eb6a
Eddie <32007214+e121016373@users.noreply.github.com>
2020-05-07 16:19:03 -0700

    connect backend and frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AppNavbar from "./components/AppNavbar";
-import { NavbarToggler } from "reactstrap";
 import ShoppingList from "./components/ShoppingList";
 import { Provider } from "react-redux";
 import store from "./store";
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { v1 as uuid } from "uuid";
 import { connect } from "react-redux";
 import { getItems, deleteItem } from "../actions/itemActions";
-import itemReducer from "../reducers/itemReducer";
 import PropTypes from "prop-types";
 
 class ShoppingList extends Component {
@@ -23,14 +21,14 @@ class ShoppingList extends Component {
 
         <ListGroup>
           <TransitionGroup className="shopping-list">
-            {items.map(({ id, name }) => (
-              <CSSTransition key={id} timeout={500} classNames="fade">
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
                   <Button
                     className="remove-btn"
                     color="danger"
                     size="sm"
-                    onClick={this.onDeleteClick.bind(this, id)}
+                    onClick={this.onDeleteClick.bind(this, _id)}
                   >
                     &times;
                   </Button>
